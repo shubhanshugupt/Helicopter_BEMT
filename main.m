d795954c0f4fa67d4c5ff2a60579bfb797f1a0e0
shubhanshugupt <gupta.shubhanshu05@gmail.com>
2021-09-30 21:50:23 +0530

    Added Plot

--- main.m
+++ main.m
@@ -8,3 +8,10 @@ for i = 1:4
     method = i;
     theta_final(i) = find_theta();
 end
+
+r = 0.2:0.01:1;
+theta_1 = theta_final(1)./r;
+theta_2 = theta_final(2)*ones(length(r));
+theta_3 = theta_final(3) + (-15)*(r-0.75);
+theta_4 = theta_final(4)./r;
+plot(r,theta_1, r, theta_2, r, theta_3, r, theta_4);
